66c7fba2eea5a7953ffc8ab0255a0f4c585e2319
abdulehsan <abdulehsan24@gmail.com>
2024-11-16 23:19:53 +0500

    updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.title('Hello World!')
+st.title("Interactive GDP Analysis Dashboard")
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Select a page", ["Home", "Visualization", "Reports"])
+
+if page == "Home":
+    st.write("Welcome to the GDP Analysis Dashboard!")
+elif page == "Visualization":
+    st.write("Navigate to Visualizations")
+    # import pages.visualization
+elif page == "Reports":
+    st.write("Navigate to Reports")
+    # import reports.report
